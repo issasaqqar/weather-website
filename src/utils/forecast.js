0f266de5186080695b79c1d47dad93f7d1f1c4d9
unknown <Administrator@JOR-AMM-L046.corp.bayt.com>
2020-02-07 23:14:35 +0200

    Update the forcast message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. The highest today is ' + body.daily.data[0].temperatureHigh+ ' while the lowest is ' + body.daily.data[0].temperatureLow)
         }
     })
 }
